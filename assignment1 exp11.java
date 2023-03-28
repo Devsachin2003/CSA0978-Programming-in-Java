f03f2d4edced15eedce3dd83445318c3ea4f5fb1
S.G.DEVSACHIN <112550872+Devsachin2003@users.noreply.github.com>
2023-03-29 00:05:16 +0530

    Create assignment1 exp11.java

---
+++ assignment1 exp11.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class hollowsquaredollar{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the side length of the square: ");
+        int side = s.nextInt();
+        
+
+        for (int i = 1; i <= side; i++) {
+            for (int j = 1; j <= side; j++) {
+                if (i == 1 || i == side || j == 1 || j == side) {
+                    System.out.print("$ ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
