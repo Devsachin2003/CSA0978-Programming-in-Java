4a37986fb67ce7fd1085b6ee2d99d3b981839354
S.G.DEVSACHIN <112550872+Devsachin2003@users.noreply.github.com>
2023-03-29 20:00:04 +0530

    Create assignment1 exp13.java

---
+++ assignment1 exp13.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class matrixmultiplication {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows in the first matrix: ");
+        int rows1 = sc.nextInt();
+        System.out.print("Enter the number of columns in the first matrix: ");
+        int cols1 = sc.nextInt();
+        int[][] mat1 = new int[rows1][cols1];
+        System.out.println("Enter the elements of the first matrix:");
+        for (int i = 0; i < rows1; i++) {
+            for (int j = 0; j < cols1; j++) {
+                mat1[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.print("Enter the number of rows in the second matrix: ");
+        int rows2 = sc.nextInt();
+        System.out.print("Enter the number of columns in the second matrix: ");
+        int cols2 = sc.nextInt();
+        int[][] mat2 = new int[rows2][cols2];
+        System.out.println("Enter the elements of the second matrix:");
+        for (int i = 0; i < rows2; i++) {
+            for (int j = 0; j < cols2; j++) {
+                mat2[i][j] = sc.nextInt();
+            }
+        }
+        if (cols1 != rows2) {
+            System.out.println("Matrix multiplication is not possible.");
+            return;
+        }
+        int[][] result = new int[rows1][cols2];
+        for (int i = 0; i < rows1; i++) {
+            for (int j = 0; j < cols2; j++) {
+                for (int k = 0; k < cols1; k++) {
+                    result[i][j] += mat1[i][k] * mat2[k][j];
+                }
+            }
+        }
+        System.out.println("Matrix Multiplication Result:");
+        for (int i = 0; i < rows1; i++) {
+            for (int j = 0; j < cols2; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
