d3ac02da36bbcaa91af03ee6494a8ed7d001f9d8
S.G.DEVSACHIN <112550872+Devsachin2003@users.noreply.github.com>
2023-03-29 23:43:10 +0530

    Create assignment2 1)exp viii).java

---
+++ assignment2 1)exp viii).java
@@ -0,0 +1,7 @@
+class lower{
+    public static void main(String[] args) {
+        String str = "The Quick Brown Fox Jumps Over The Lazy Dog";
+        String result = str.toLowerCase();
+        System.out.println(result);
+    }
+}
