3f64ba5662b208beed9a5cef1f6df51e5e76c1a8
S.G.DEVSACHIN <112550872+Devsachin2003@users.noreply.github.com>
2023-03-29 23:31:15 +0530

    Create assignment2 1)exp v).java

---
+++ assignment2 1)exp v).java
@@ -0,0 +1,10 @@
+class Rangesub{
+    public static void main(String[] args) {
+        String str = "The quick brown fox jumps over the lazy dog";
+        int start = 10;
+        int end = 15;
+        
+        String result = str.substring(start, end);
+        System.out.println(result);
+    }
+}
