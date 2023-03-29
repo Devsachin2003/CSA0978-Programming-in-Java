864ac798994b44147a0c5aedab36129d96f74469
S.G.DEVSACHIN <112550872+Devsachin2003@users.noreply.github.com>
2023-03-29 23:37:59 +0530

    Create assignment2 1)exp vii).java

---
+++ assignment2 1)exp vii).java
@@ -0,0 +1,7 @@
+class Trim{
+    public static void main(String[] args) {
+        String str = "   The quick brown fox jumps over the lazy dog   ";
+        String result = str.trim();
+        System.out.println(result);
+    }
+}
